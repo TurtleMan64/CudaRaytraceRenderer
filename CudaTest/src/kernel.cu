423eb740f9a4c1625d6c6110e9ed7e66ba88db88
TurtleMan64 <turtleboy1026@aol.com>
2021-05-23 23:57:11 -0700

    Add Orca scene and readme

--- CudaTest/src/kernel.cu
+++ CudaTest/src/kernel.cu
@@ -156,6 +156,28 @@ __device__ void interpolateUvTriangle(float out[],
     out[1] = uv1y*a1 + uv2y*a2 + uv3y*a3;
 }
 
+__device__ void barryUvToUv(const int triIdx, const float bu, const float bv, float* outU, float* outV)
+{
+    int base = triIdx*96;
+
+    float uv1x, uv1y, uv2x, uv2y, uv3x, uv3y;
+    memcpy(&uv1x, &trianglesGPU[72+base], 4); //uv
+    memcpy(&uv1y, &trianglesGPU[76+base], 4);
+    memcpy(&uv2x, &trianglesGPU[80+base], 4);
+    memcpy(&uv2y, &trianglesGPU[84+base], 4);
+    memcpy(&uv3x, &trianglesGPU[88+base], 4);
+    memcpy(&uv3y, &trianglesGPU[92+base], 4);
+
+    float bw = 1-bu-bv;
+
+    float a1 = -bw;
+    float a2 = -bu;
+    float a3 = -bv;
+
+    *outU = -(uv1x*a1 + uv2x*a2 + uv3x*a3);
+    *outV =   uv1y*a1 + uv2y*a2 + uv3y*a3;
+}
+
 __device__ bool checkPointInTriangle2DGPU(
 	const float x,  const float y,
 	const float x1, const float y1,
@@ -203,7 +225,7 @@ __device__ int sampleTexture(const float u, const float v, const int textureId, 
     }
 }
 
-__device__ void sampleTriangleUvGPU(const float cx, const float cy, const float cz, const int triIdx, float* u, float* v)
+__device__ void sampleTriangleUvGPU(const float cx, const float cy, const float cz, const int triIdx, float* u, float* v, bool debug)
 {
     int base = triIdx*96;
     float t1x, t1y, t1z, t2x, t2y, t2z, t3x, t3y, t3z, uv1x, uv1y, uv2x, uv2y, uv3x, uv3y;
@@ -301,9 +323,90 @@ __device__ void calculateTangentSpaceOfUv(const int triIdx, float* outTangent, f
     outBitangent[2] = bitangent[2];
 }
 
-__device__ void rotateToTangentGPU(float textureNormal[], float tangent[])
+__device__ bool checkCollision2GPU(char* result,
+                                   const float px1, const float py1, const float pz1,
+                                   const float px2, const float py2, const float pz2)
 {
-    
+    //float R[] = {px1, py1, pz1};
+    float Rdir[] = {px2-px1, py2-py1, pz2-pz1};
+    vec3normalize(Rdir);
+
+    int numTriangles = 0;
+    memcpy(&numTriangles, trianglesGPU, 4);
+
+    float minDist = 100000000000.0f;
+    int triangleId = -1;
+    float cPos[] = {0,0,0};
+    float bubv[] = {0,0};
+
+    //int i = blockIdx.x;
+    int i = -1;
+    for (int count = 0; count < numTriangles; count++)
+    {
+        //i = (i+1) % numTriangles;
+        i++;
+
+        int base = i*96;
+        float t1x, t1y, t1z, t2x, t2y, t2z, t3x, t3y, t3z;
+        memcpy(&t1x, &trianglesGPU[ 4+base], 4);
+        memcpy(&t1y, &trianglesGPU[ 8+base], 4);
+        memcpy(&t1z, &trianglesGPU[12+base], 4);
+        memcpy(&t2x, &trianglesGPU[16+base], 4);
+        memcpy(&t2y, &trianglesGPU[20+base], 4);
+        memcpy(&t2z, &trianglesGPU[24+base], 4);
+        memcpy(&t3x, &trianglesGPU[28+base], 4);
+        memcpy(&t3y, &trianglesGPU[32+base], 4);
+        memcpy(&t3z, &trianglesGPU[36+base], 4);
+
+        //float A[] = {t1x, t1y, t1z};
+        //float B[] = {t2x, t2y, t2z};
+        //float C[] = {t3x, t3y, t3z};
+
+        float E1[] = {t2x-t1x, t2y-t1y, t2z-t1z};
+        float E2[] = {t3x-t1x, t3y-t1y, t3z-t1z};
+
+        float N[] = {0,0,0};
+        vec3cross(N, E1, E2);
+
+        float det = -(vec3dot(Rdir, N));
+        float invdet = 1.0f/det;
+
+        float AO[] = {px1-t1x, py1-t1y, pz1-t1z};
+        float DAO[] = {0,0,0};
+        vec3cross(DAO, AO, Rdir);
+
+        float u =  vec3dot(E2, DAO)*invdet;
+        float v = -vec3dot(E1, DAO)*invdet;
+        float t =  vec3dot(AO,   N)*invdet;
+
+        bool intersects = (det >= 1e-6f && t >= 0.0f && u >= 0.0f && v >= 0.0f && (u+v) <= 1.0f);
+
+        if (intersects && t < minDist)
+        {
+            cPos[0] = px1 + t*Rdir[0];
+			cPos[1] = py1 + t*Rdir[1];
+			cPos[2] = pz1 + t*Rdir[2];
+
+            bubv[0] = u;
+            bubv[1] = v;
+
+			minDist = t;
+            triangleId = i;
+		}
+    }
+
+    if (triangleId >= 0)
+    {
+        memcpy(&result[ 0], &triangleId, 4);
+        memcpy(&result[ 4], &cPos[0], 4);
+        memcpy(&result[ 8], &cPos[1], 4);
+        memcpy(&result[12], &cPos[2], 4);
+        memcpy(&result[16], &bubv[0], 4);
+        memcpy(&result[20], &bubv[1], 4);
+        return true;
+    }
+
+    return false;
 }
 
 __device__ bool checkCollisionGPU(char* result,
@@ -423,7 +526,7 @@ __device__ int doPixelGPU(const float* camValues, const int screenX, const int s
     gazePosition[1] += worldCastDirection[1];
     gazePosition[2] += worldCastDirection[2];
 
-    char result[] = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
+    char result[] = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
 
     //if (screenX == 1600/2 && screenY == 900/2)
     {
@@ -437,18 +540,23 @@ __device__ int doPixelGPU(const float* camValues, const int screenX, const int s
         {
             //printf("v %f %f %f\nv %f %f %f\n", startPosition[0], startPosition[1], startPosition[2], gazePosition[0], gazePosition[1], gazePosition[2]);
         }
-        if (checkCollisionGPU(result, startPosition[0], startPosition[1], startPosition[2], gazePosition[0], gazePosition[1], gazePosition[2]))
+        if (checkCollision2GPU(result, startPosition[0], startPosition[1], startPosition[2], gazePosition[0], gazePosition[1], gazePosition[2]))
         {
             int triIdx;
-            float colX, colY, colZ;
+            float colX, colY, colZ, bu, bv;
 
             memcpy(&triIdx, &result[ 0], 4);
             memcpy(&colX,   &result[ 4], 4);
             memcpy(&colY,   &result[ 8], 4);
             memcpy(&colZ,   &result[12], 4);
+            memcpy(&bu,     &result[16], 4);
+            memcpy(&bv,     &result[20], 4);
+
+            //bool debug = (screenX == 1600/2 && screenY == 900/2);
 
             float u, v;
-            sampleTriangleUvGPU(colX, colY, colZ, triIdx, &u, &v);
+            //sampleTriangleUvGPU(colX, colY, colZ, triIdx, &u, &v);
+            barryUvToUv(triIdx, bu, bv, &u, &v);
 
             int base = triIdx*96;
 
@@ -481,26 +589,26 @@ __device__ int doPixelGPU(const float* camValues, const int screenX, const int s
                 float tangent[] = {0,0,0};
                 float bitangent[] = {0,0,0};
                 calculateTangentSpaceOfUv(triIdx, tangent, bitangent);
-                if (screenX == 1600/2 && screenY == 900/2)
+                //if (screenX == 1600/2 && screenY == 900/2)
                 {
                     //printf("%f %f %f       ", tangent[0], tangent[1], tangent[2]);
                     //printf("%f %f %f\n", bitangent[0], bitangent[1], bitangent[2]);
                 }
 
-                float normal[] = {0,0,0};
-                memcpy(&normal[0], &trianglesGPU[56+base], 4); //normal
-                memcpy(&normal[1], &trianglesGPU[60+base], 4);
-                memcpy(&normal[2], &trianglesGPU[64+base], 4);
+                float normalTriangle[] = {0,0,0};
+                memcpy(&normalTriangle[0], &trianglesGPU[56+base], 4); //normal
+                memcpy(&normalTriangle[1], &trianglesGPU[60+base], 4);
+                memcpy(&normalTriangle[2], &trianglesGPU[64+base], 4);
 
                 float newNormLen = (vec3len(tangent) + vec3len(bitangent))/2;
-                normal[0]*=newNormLen;
-                normal[1]*=newNormLen;
-                normal[2]*=newNormLen;
+                normalTriangle[0]*=newNormLen;
+                normalTriangle[1]*=newNormLen;
+                normalTriangle[2]*=newNormLen;
 
                 float normalWorldSpace[] = {0,0,0};
-                normalWorldSpace[0] = tangent[0]*tangentNormal[0] + bitangent[0]*tangentNormal[1] + normal[0]*tangentNormal[2];
-                normalWorldSpace[1] = tangent[1]*tangentNormal[0] + bitangent[1]*tangentNormal[1] + normal[1]*tangentNormal[2];
-                normalWorldSpace[2] = tangent[2]*tangentNormal[0] + bitangent[2]*tangentNormal[1] + normal[2]*tangentNormal[2];
+                normalWorldSpace[0] = tangent[0]*tangentNormal[0] + bitangent[0]*tangentNormal[1] + normalTriangle[0]*tangentNormal[2];
+                normalWorldSpace[1] = tangent[1]*tangentNormal[0] + bitangent[1]*tangentNormal[1] + normalTriangle[1]*tangentNormal[2];
+                normalWorldSpace[2] = tangent[2]*tangentNormal[0] + bitangent[2]*tangentNormal[1] + normalTriangle[2]*tangentNormal[2];
 
                 //if (screenX == 1600/2 && screenY == 900/2)
                 {
@@ -522,6 +630,13 @@ __device__ int doPixelGPU(const float* camValues, const int screenX, const int s
                 vec3normalize(newDirection);
                 vec3normalize(normalWorldSpace);
                 vec3bounce(newDirection, normalWorldSpace);
+
+                vec3normalize(normalTriangle);
+                if (vec3dot(newDirection, normalTriangle) < 0.0f) //if we reflect "inside" the triangle, flip it to outside
+                {
+                    vec3bounce(newDirection, normalTriangle);
+                    //return pixelFromRgbGPU(1, 0, 0);
+                }
                 
                 startPosition[0] = colX + 0.001f*newDirection[0];
                 startPosition[1] = colY + 0.001f*newDirection[1];
@@ -677,6 +792,8 @@ int main(int argc, char* argv[])
     double timeSeconds = timeMillis/1000.0;
     printf("%f fps\n", frameCount/timeSeconds);
     Uint32 lastTimestamp = SDL_GetTicks();
+    Uint32 lastSecond = SDL_GetTicks();
+    int fpsCount = 0;
 
     bool conti = true;
     while (conti)
@@ -685,6 +802,13 @@ int main(int argc, char* argv[])
         Uint32 newTimestamp = SDL_GetTicks();
         float dt = (newTimestamp - lastTimestamp)/1000.0f;
         lastTimestamp = newTimestamp;
+        fpsCount++;
+        if (newTimestamp - lastSecond >= 1000)
+        {
+            printf("fps = %d\n", fpsCount);
+            fpsCount = 0;
+            lastSecond = newTimestamp;
+        }
 
         SDL_Event windowEvent;
         while (SDL_PollEvent(&windowEvent))
@@ -756,7 +880,7 @@ int main(int argc, char* argv[])
 			camPosition.y+= speed*dt;
 		}
 
-        printf("fps = %f\n", 1/dt);
+        //printf("fps = %f\n", 1/dt);
 
         render();
 
@@ -941,7 +1065,7 @@ cudaError_t setupGlobalVars()
         return cudaStatus;
     }
 
-    CollisionModel* model = loadCollisionModel("res/", "sky2.obj");
+    CollisionModel* model = loadCollisionModel("res/", "Orca.obj");
     std::vector<char> buf = model->serializeTrianglesToGPU();
     setupTextures(model);
     //trianglesGPU = new char[(int)buf.size()];
@@ -1043,7 +1167,7 @@ __device__  void rotatePointGPU(float result[],
     float y = point[1];
     float z = point[2];
 
-    if (sqrt(u*u + v*v + w*w) < 0.000000001)
+    if (sqrt(u*u + v*v + w*w) < 0.000000001f)
 	{
 		//printf("Warning: trying to rotate by a very small axis [%f %f %f]\n", u, v, w);
 		result[0] = x;
@@ -1051,10 +1175,6 @@ __device__  void rotatePointGPU(float result[],
 		result[2] = z;
 		return;
 	}
-
-    float a = 0;
-    float b = 0;
-    float c = 0;
 
 	float u2 = u*u;
 	float v2 = v*v;
@@ -1066,17 +1186,17 @@ __device__  void rotatePointGPU(float result[],
 	float oneMinusCosT = 1 - cosT;
 	float sinT = sin(theta);
 
-	result[0] = ((a*(v2 + w2) - u*(b*v + c*w - u*x - v*y - w*z)) * oneMinusCosT
-		+ l2*x*cosT
-		+ l*(-c*v + b*w - w*y + v*z)*sinT) / l2;
+	result[0] = ((-u*(-u*x - v*y - w*z)) * oneMinusCosT
+        + l2*x*cosT
+		+ l*(-w*y + v*z)*sinT) / l2;
 
-	result[1] = ((b*(u2 + w2) - v*(a*u + c*w - u*x - v*y - w*z)) * oneMinusCosT
+	result[1] = ((-v*(-u*x - v*y - w*z)) * oneMinusCosT
 		+ l2*y*cosT
-		+ l*(c*u - a*w + w*x - u*z)*sinT) / l2;
+		+ l*( w*x - u*z)*sinT) / l2;
 
-	result[2] = ((c*(u2 + v2) - w*(a*u + b*v - u*x - v*y - w*z)) * oneMinusCosT
+	result[2] = ((-w*(-u*x - v*y - w*z)) * oneMinusCosT
 		+ l2*z*cosT
-		+ l*(-b*u + a*v - v*x + u*y)*sinT) / l2;
+		+ l*(-v*x + u*y)*sinT) / l2;
 }
 
 //out is vector 3 of float
---
+++ README.md
@@ -0,0 +1,10 @@
+#CUDA Raytrace Renderer
+
+* A program that is capable of rendering a 3D model in realtime, without using traditional 3D rendering APIs (OpenGL, DirectX, etc.)
+* Uses CUDA to cast each screen pixel as a ray in parallel on the graphics card. This allows the ~2 million ray casts per frame to happen in realtime.
+* Supports up to ~330 triangles.
+* Can define mirror type materials. Mirrors reflect incoming rays, as defined by a normal map.
+* Uses SDL2 to render final image to screen.
+
+##Example scene:
+![Orca over ocean](https://github.com/TurtleMan64/CudaRaytraceRenderer/blob/main/Example.png?raw=true)
